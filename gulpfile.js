2394143296505aaead700bf542a68989c7f445af
AlanCarlosSS <acss873@gmail.com>
2024-05-06 14:34:19 -0300

    correções do exercicio

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,20 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const terser = require('gulp-terser');
 
-function compilarSass() {
-  return gulp.src('./source/styles/**/*.scss')
-    .pipe(sass())
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
-}
+});
 
-function comprimirImagens() {
-  return gulp.src('./source/images/**/*')
+gulp.task('imagemin', function () {
+  return gulp.src('./src/images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'));
-}
+});
 
-function comprimirJavaScript() {
-  return gulp.src('./source/scripts/**/*.js')
+gulp.task('compress', function () {
+  return gulp.src('./src/js/*.js')
     .pipe(terser())
-    .pipe(gulp.dest('./dist/scripts'));
-}
-
-function observar() {
-  gulp.watch('./source/styles/**/*.scss', { ignoreInitial: false }, gulp.series(compilarSass));
-  gulp.watch('./source/scripts/**/*.js', { ignoreInitial: false }, gulp.series(comprimirJavaScript));
-  gulp.watch('./source/images/**/*', { ignoreInitial: false }, gulp.series(comprimirImagens));
-}
-
-exports.default = observar;
+    .pipe(gulp.dest('./dist/js'));
+});
